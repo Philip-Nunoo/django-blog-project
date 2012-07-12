bbafb5a40c5876d03254f727b04ef4e6a1f70669
AITI Student <aiti-student@aiti-079-PC.(none)>
2012-07-12 11:15:28 -0400

    blog

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -1,7 +1,7 @@
 # Django settings for myblog project.
 import os
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
-#SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 import dj_database_url
 
 DEBUG = True
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -66,6 +66,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'assets'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
